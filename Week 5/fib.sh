f8f3898573f314288fbb436d095969bba2a8f377
kp10-x <krishnaprasadv8@gmail.com>
2020-11-03 11:12:05 +0530

    Lab 5

---
+++ Week 5/fib.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+echo "Enter N"
+read n
+x=0
+y=1
+i=2
+echo "Fibonacci Series up to $n terms :"
+echo "$x"
+echo "$y"
+while [ $i -lt $n ]
+do
+i=`expr $i + 1 `
+z=`expr $x + $y `
+echo "$z"
+x=$y
+y=$z
+done
---
+++ Week 5/tempconversion.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Enter temperature (F) : "
+read tf
+tc=$(echo "scale=2;(5/9)*($tf-32)"|bc)
+echo "$tf = $tc"
